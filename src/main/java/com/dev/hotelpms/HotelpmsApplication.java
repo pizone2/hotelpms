06c8e0b23e06b1571c23652d120a7197072d02b8
10000JI <sksk436@naver.com>
2023-05-22 14:19:04 +0900

    setting

---
+++ src/main/java/com/dev/hotelpms/HotelpmsApplication.java
@@ -0,0 +1,13 @@
+package com.dev.hotelpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelpmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/hotelpms/HotelpmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.hotelpms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelpmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
