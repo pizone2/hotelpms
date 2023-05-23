14f5e1986bd0459c8f19124de3a9f2df00316c51
jinjinzala <jinjinzala@naver.com>
2023-05-23 17:34:52 +0900

    heejinmaster20230523

--- src/main/java/com/dev/hotelpms/booking/BookingVO.java
+++ src/main/java/com/dev/hotelpms/booking/BookingVO.java
@@ -1,7 +1,12 @@
 package com.dev.hotelpms.booking;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Setter
+@Getter
 public class BookingVO {
      private  String reservationNumber;
      private  Long roomNumber;
@@ -15,102 +20,6 @@ public class BookingVO {
      private Date checkoutDate;
      private String reservationStatus;
      private Long paymentAmount;
-
-     public String getReservationNumber() {
-          return reservationNumber;
-     }
-
-     public void setReservationNumber(String reservationNumber) {
-          this.reservationNumber = reservationNumber;
-     }
-
-     public Long getRoomNumber() {
-          return roomNumber;
-     }
-
-     public void setRoomNumber(Long roomNumber) {
-          this.roomNumber = roomNumber;
-     }
-
-     public String getId() {
-          return id;
-     }
-
-     public void setId(String id) {
-          this.id = id;
-     }
-
-     public String getRoomType() {
-          return roomType;
-     }
-
-     public void setRoomType(String roomType) {
-          this.roomType = roomType;
-     }
-
-     public String getName() {
-          return name;
-     }
-
-     public void setName(String name) {
-          this.name = name;
-     }
-
-     public String getReservationEmail() {
-          return reservationEmail;
-     }
-
-     public void setReservationEmail(String reservationEmail) {
-          this.reservationEmail = reservationEmail;
-     }
-
-     public String getGuestCount() {
-          return guestCount;
-     }
-
-     public void setGuestCount(String guestCount) {
-          this.guestCount = guestCount;
-     }
-
-     public Date getPaymentDate() {
-          return paymentDate;
-     }
-
-     public void setPaymentDate(Date paymentDate) {
-          this.paymentDate = paymentDate;
-     }
-
-     public Date getCheckinDate() {
-          return checkinDate;
-     }
-
-     public void setCheckinDate(Date checkinDate) {
-          this.checkinDate = checkinDate;
-     }
-
-     public Date getCheckoutDate() {
-          return checkoutDate;
-     }
-
-     public void setCheckoutDate(Date checkoutDate) {
-          this.checkoutDate = checkoutDate;
-     }
-
-     public String getReservationStatus() {
-          return reservationStatus;
-     }
-
-     public void setReservationStatus(String reservationStatus) {
-          this.reservationStatus = reservationStatus;
-     }
-
-     public Long getPaymentAmount() {
-          return paymentAmount;
-     }
-
-     public void setPaymentAmount(Long paymentAmount) {
-          this.paymentAmount = paymentAmount;
-     }
 
 
 
--- src/main/java/com/dev/hotelpms/room/RoomTypeVO.java
+++ src/main/java/com/dev/hotelpms/room/RoomTypeVO.java
@@ -1,33 +1,16 @@
 package com.dev.hotelpms.room;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class RoomTypeVO {
     private String roomType;
     private  Long capacity;
     private  Long roomPrice;
 
-    public String getRoomType() {
-        return roomType;
-    }
 
-    public void setRoomType(String roomType) {
-        this.roomType = roomType;
-    }
-
-    public Long getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(Long capacity) {
-        this.capacity = capacity;
-    }
-
-    public Long getRoomPrice() {
-        return roomPrice;
-    }
-
-    public void setRoomPrice(Long roomPrice) {
-        this.roomPrice = roomPrice;
-    }
 
 
 
