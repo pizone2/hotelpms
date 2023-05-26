2d4945088f5250ea8ea43499907f5ff81267ecf5
10000JI <sksk436@naver.com>
2023-05-26 09:22:07 +0900

    reserved

--- src/main/java/com/dev/hotelpms/room/BookingDAO.java
+++ src/main/java/com/dev/hotelpms/room/BookingDAO.java
@@ -1,10 +1,11 @@
 package com.dev.hotelpms.room;
 
+import com.dev.hotelpms.user.UserVO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface BookingDAO {
+    public UserVO getSucessReserve() throws Exception;
 
-
-
+    public UserVO getPossibleReserve() throws Exception;
 }
--- src/main/java/com/dev/hotelpms/room/BookingService.java
+++ src/main/java/com/dev/hotelpms/room/BookingService.java
@@ -1,6 +1,7 @@
 package com.dev.hotelpms.room;
 
 
+import com.dev.hotelpms.user.UserVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,10 @@ public class BookingService {
     @Autowired
     private BookingDAO bookingDAO;
 
+    public UserVO getSucessReserve(UserVO userVO) throws Exception{
+        userVO = bookingDAO.getSucessReserve();
+        return userVO;
+    }
 
 
 }
