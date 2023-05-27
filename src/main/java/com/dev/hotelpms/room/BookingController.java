fbf39447e1cdbe11e2a0db7842dc62f092e774d4
10000JI <sksk436@naver.com>
2023-05-27 18:15:31 +0900

    test

--- src/main/java/com/dev/hotelpms/room/BookingController.java
+++ src/main/java/com/dev/hotelpms/room/BookingController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RequestMapping("/booking/*")
 @Slf4j
 public class BookingController {
-//테스트ㄴㅁ
+//테스트ㄴㅁASDFSDF
     @Autowired
     private BookingService bookingService;
 
